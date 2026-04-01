721d376104f7cd881421b0bcff60806589322b12
omarfarah-atlan <omarfarah-atlan@users.noreply.github.com>
2026-04-01 16:43:15 -0400

    test: trigger sql impact analysis

--- queries/revenue.sql
+++ queries/revenue.sql
@@ -9,3 +9,4 @@ JOIN catalog.products p ON p.id = o.product_id
 WHERE o.created_at >= '2024-01-01'
 GROUP BY 1, 2, 3
 LEFT JOIN finance.invoices i ON i.order_id = o.id
+LEFT JOIN finance.invoices i ON i.order_id = o.id
